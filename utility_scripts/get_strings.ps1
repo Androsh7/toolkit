337e4de0b1b20a38c27d74d1cb4f5ec8dbd66fe3
androsh7@gmail.com <androsh7@gmail.com>
2026-02-28 00:56:16 -0500

    Fixed get_strings.ps1

--- utility_scripts/get_strings.ps1
+++ utility_scripts/get_strings.ps1
@@ -1,52 +1,34 @@
-Add-Type -AssemblyName System.Windows.Forms
-
 $Host.UI.RawUI.WindowTitle = "Get Strings"
+Write-Host "Running Get_Strings.ps1 at $(Get-Date)" -ForegroundColor Cyan
 
-Write-Host "Running Get_Strings.ps1 at $(Get-Date)"-ForegroundColor Cyan
+# Select input/output file
+$selectedFile = Read-Host -Prompt "Input file"
+$out_file = Read-Host -Prompt "Output file"
 
-$out_file = "${env:TEMP}\Strings.txt"
+# Minimum length
+$min_string_len = Read-Host "Select the minimum string length (default 3)"
+if ([string]::IsNullOrWhiteSpace($min_string_len)) { $min_string_len = 3 }
+$min_string_len = [int]$min_string_len
+if ($min_string_len -lt 1) { $min_string_len = 3 }
 
-# opens a prompt to select a file
-Write-Host "Please select a file"
-$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$OpenFileDialog.InitialDirectory = [Environment]::GetFolderPath('Desktop')
-$OpenFileDialog.Filter = "All files (*.*)|*.*"
-$OpenFileDialog.Multiselect = $false
-
-if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
-    $selectedFile = $OpenFileDialog.FileName
-    Write-Host "Selected file: $selectedFile" -ForegroundColor Cyan
-} else {
-    Write-Host "No file selected." -ForegroundColor Red
-    Exit
+$bytes = [System.IO.File]::ReadAllBytes($selectedFile)
+@(
+    "GetStrings.ps1 running on $(Get-Date)"
+    "Parsing File: `"$selectedFile`""
+    "Minimum String Length: $min_string_len"
+    "------------------------------------------------------------------------------"
+) | Set-Content -LiteralPath $out_file -Encoding ascii
+$temp_string = ""
+Write-Host "Parsing file, this may take a while..." -ForegroundColor Green
+$bytes | foreach-object {
+    if ($_ -ge 32 -and $_ -le 126) {
+        $temp_string += [char]$_
+    } else {
+        if ($temp_string.Length -ge $min_string_len) {
+            Out-File -InputObject $temp_string -FilePath $out_file -Append -Encoding ascii
+        }
+        $temp_string = ""
+    }
 }
 
-# prompts the user to select the minimum string length
-[int]$min_string_len = Read-Host "Select the minimum string length (default 3)"
-if ($null -eq $min_string_len) { $min_string_len = 3 }
-
-# tests to ensure the file exists
-if (Test-Path $selectedFile) {
-    Write-Host "File is accessible, proceeding to parse for strings" -ForegroundColor Green
-    $Host.UI.RawUI.WindowTitle = "GetStrings - $selectedFile"
-} else {
-    Write-Host "File is inaccessible, verify you have permissions to read this file" -ForegroundColor Red
-    Read-Host "`nPress ENTER to exit"
-    Exit
-}
-
-# add output file headers
-"GetStrings.ps1 running on $(Get-Date)" > $out_file
-"Parsing File: `"$selectedFile`"" >> $out_file
-"Minimum String Length: $min_string_len" >> $out_file
-"This file is saved in $out_file" >> $out_file
-"------------------------------------------------------------------------------" >> $out_file
-
-[System.IO.File]::ReadAllLines($selectedFile) |
-    ForEach-Object {
-        [regex]::Matches($_, "[\x20-\x7E]{${min_string_len},}") |
-            ForEach-Object { $_.Value }
-    } |
-    Tee-Object -FilePath $out_file -Append
-
-Start-Process -FilePath "Notepad.exe" -ArgumentList $out_file
+Write-Host "Wrote output to $out_file" -ForegroundColor Green
